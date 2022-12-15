60ca4cf1a5f5a93318c1a689bc04aa8854b34cd5
Ops0 <103710208+Os7ama@users.noreply.github.com>
2022-12-15 20:29:29 +0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,45 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+// buotton up 
+let btn = document.querySelector("button");
+
+window.onscroll = function () {
+    if(window.scrollY >= 600) {
+        btn.style.display = "block";
+    }else{
+        btn.style.display = "none";
+    }
+};
+btn.onclick = function() {
+    window.scrollTo({
+        left :0,
+        top:0,
+        behavior:"smooth",
+    })
+};
